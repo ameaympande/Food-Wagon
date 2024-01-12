06bb8284cd2716c1c402c3aaa2fe8bc927b476d7
Ameay Pande <ameay35@gmail.com>
2024-01-12 13:12:30 +0530

    added ordersection

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,17 +5,18 @@ import OfferSection from "./homePage/OfferSection";
 import HDITsection from "./homePage/HDITsection";
 import PopularItemSection from "./homePage/PopularItemSection";
 import FeatureRestaurant from "./homePage/FeatureRestaurant";
+import OrderSection from "./homePage/OrderSection";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-
       <Body />
       <OfferSection />
       <HDITsection />
       <PopularItemSection />
       <FeatureRestaurant />
+      <OrderSection />
     </>
   );
 };
